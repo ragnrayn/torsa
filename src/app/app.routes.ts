24256ce69728e7d081bae9e2feb7f48891f5aa24
Arthur Kravits <ragnrayn@Noutbuk-Arthur.datanet.kiev.ua>
2024-06-26 21:20:57 +0300

    almost done with hero section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "home", loadComponent: () => import("./pages/home/home.component").then((x) => x.HomeComponent) },
+  { path: "**", redirectTo: "/home" }
+];
